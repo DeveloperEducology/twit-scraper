ec610a46a80ab750465ac2e85adc4e0668831985
vijay marka <111419184+DeveloperEducology@users.noreply.github.com>
2025-09-06 16:35:25 +0530

    Update render-build.sh

--- render-build.sh
+++ render-build.sh
@@ -1,10 +1,17 @@
+# !/usr/bin/env bash
+# exit on error
+# set -o errexit
+
+# Install Python dependencies from requirements.txt
+# pip install -r requirements.txt
+
+# Install the Chromium browser required by Playwright
+# playwright install chromium
+
+
 #!/usr/bin/env bash
 # exit on error
 set -o errexit
 
-# Install Python dependencies from requirements.txt
 pip install -r requirements.txt
-
-# Install the Chromium browser required by Playwright
 playwright install chromium
-
